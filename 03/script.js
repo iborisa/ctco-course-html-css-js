c7d3d76b102684c890aa4cd98d53f14edc6cbccd
S7QKFM <January2021>
2022-09-23 09:06:59 +0300

    HM 1

--- 03/script.js
+++ 03/script.js
@@ -1,12 +1,12 @@
 const name = 'Java';
 
 function displayName() {
-  const name = 'Script';
-  logMessage(name);
+    const name = 'Script';
+    logMessage(name);
 }
 
 function logMessage(name) {
-  console.log(`Hello ${name}`);
+    console.log(`Hello ${name}`);
 }
 
 displayName();
@@ -16,6 +16,20 @@ linkElement.setAttribute('target', '_blank');
 
 document.querySelector('h1').style.color = '#5cdc5f';
 
-document.addEventListener('click', function(event) {
-  console.log(`Click detected x ${event.offsetX} y ${event.offsetY}`);
+let click = 0;
+document.addEventListener('click', function (event) {
+    click += 1;
+    if (click === 5) {
+        alert('Alert!')
+    }
 });
+
+document.getElementById("intro").innerHTML = new Date().getFullYear();
+
+const joinUs = document.querySelector("#link");
+
+joinUs.addEventListener("click", function () {
+    joinUs.classList.toggle("is-hidden")
+});
+
+
